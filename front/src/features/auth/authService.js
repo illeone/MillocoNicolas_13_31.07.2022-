7d4e8525376e17cbc576073ec9cae8c4918fb5f3
Nicolas <nicolasmilloco@hotmail.com>
2023-06-26 16:25:32 +0200

    fonctionnalité de stockage local pour la réponse de connexion

--- front/src/features/auth/authService.js
+++ front/src/features/auth/authService.js
@@ -2,7 +2,12 @@ import axios from "axios"
 
 const login = async(userData) => {
     const response = await axios.post("http://localhost:3001/api/v1/user/login", userData)
+    if(response.data){
+        localStorage.setItem("userLogin",JSON.stringify(response.data))
+    }
     return response.data 
 }
+
+const authService = {login}
 
 export default authService
