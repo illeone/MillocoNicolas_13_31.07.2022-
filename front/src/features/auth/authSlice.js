da7eae906e1b6d1ee55d7ecbd3e689b698c7bdae
Nicolas <nicolasmilloco@hotmail.com>
2023-06-30 18:51:44 +0200

    Ajout de l'action asynchrone de login dans authSlice.js
     vérifie les informations de l'utilisateur

--- front/src/features/auth/authSlice.js
+++ front/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import {createSlice, createAsyncThunk} from "@reduxjs/toolkit"
+import authService from "./authService"
 
 //get userLogin from localStorage
 const userLogin = JSON.parse(localStorage.getItem("userLogin"))
@@ -11,6 +12,17 @@ const initialState = {
     message:"",
     userInfos: null,
 }
+
+export const login = createAsyncThunk("auth/login", async(user,thunkAPI) => {
+    try {
+        return await authService.login(user)
+        
+    } catch (error) {
+        const message = error.response.data.message
+        return thunkAPI.rejectWithValue(message)
+    }
+}) 
+
 
 
 export const authSlice = createSlice({
